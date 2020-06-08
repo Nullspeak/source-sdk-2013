5777cd88496667a9afa3d0ff2e2fd7a255beab40
Sarah <metalflakecobaltpaint@gmail.com>
2020-06-08 14:14:42 +1000

    Update pf_player.hxx

--- mp/src/game/shared/pagefault/pf_player.hxx
+++ mp/src/game/shared/pagefault/pf_player.hxx
@@ -16,14 +16,24 @@
 class CPFPlayer : public CBasePlayer {
 public:
 	DECLARE_CLASS(CPFPlayer, CBasePlayer);
+	DECLARE_NETWORKCLASS();
 
 	virtual void Spawn(void);
 	virtual void PreThink(void);
 
 #ifdef GAME_DLL
 	virtual void CreateViewModel(int viewmodelindex = 0);
+
+	//void SetInventoryItem(CPFPropItem *item);
+	//void DropInventoryItem(void);
 #endif
 private:
 	bool m_bSprinting{false};
+
+#ifdef CLIENT_DLL
+	EntityHandle_t m_hInventoryItem;
+#else
+	CNetworkHandle(EntityHandle_t, m_hInventoryItem);
+#endif
 };
 #endif /* PF_PLAYER_HXX */
